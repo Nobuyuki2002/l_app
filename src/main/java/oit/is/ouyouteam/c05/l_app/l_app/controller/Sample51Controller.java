1c66ede5c808822a570d090212ad762526505496
e1b20095 <e1b20095@oit.ac.jp>
2022-10-25 15:07:22 +0900

    途中

--- src/main/java/oit/is/ouyouteam/c05/l_app/l_app/controller/Sample51Controller.java
+++ src/main/java/oit/is/ouyouteam/c05/l_app/l_app/controller/Sample51Controller.java
@@ -2,13 +2,9 @@ package oit.is.ouyouteam.c05.l_app.l_app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is.ouyouteam.c05.l_app.l_app.model.FruitMapper;
 
@@ -20,8 +16,8 @@ public class Sample51Controller {
   FruitMapper fruitMapper;
 
   @GetMapping("step1")
-  public String sample21() {
-    model.addAttribute("fruits", fruitMapper.selectAllByFruit());
+  public String sample21(ModelMap model) {
+    model.addAttribute("fruits", fruitMapper.selectAllFruit());
     return "sample51.html";
   }
 
