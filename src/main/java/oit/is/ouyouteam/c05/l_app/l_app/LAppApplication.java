9442d6c98b4679c9389e3ec7093a7daa9d16ab52
Nobuyuki2002 <e1b20066@oit.ac.jp>
2022-10-25 13:56:06 +0900

    first commit

---
+++ src/main/java/oit/is/ouyouteam/c05/l_app/l_app/LAppApplication.java
@@ -0,0 +1,13 @@
+package oit.is.ouyouteam.c05.l_app.l_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/ouyouteam/c05/l_app/l_app/LAppApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.ouyouteam.c05.l_app.l_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
