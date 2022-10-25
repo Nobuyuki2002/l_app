c36907bbf11f0b08b735e47a26b062e6cc248950
Nobuyuki2002 <e1b20066@oit.ac.jp>
2022-10-25 16:17:39 +0900

    編集機能

--- src/main/java/oit/is/ouyouteam/c05/l_app/l_app/controller/Sample51Controller.java
+++ src/main/java/oit/is/ouyouteam/c05/l_app/l_app/controller/Sample51Controller.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -39,6 +40,33 @@ public class Sample51Controller {
     // 削除後のフルーツリストを取得
     ArrayList<Fruit> fruits = fruitMapper.selectAllFruit();
     model.addAttribute("fruits", fruits);
+    return "sample51.html";
+  }
+
+  @GetMapping("step4")
+  @Transactional
+  public String sample54(@RequestParam Integer id, ModelMap model) {
+    Fruit update_fruit = fruitMapper.selectById(id);
+    model.addAttribute("update_fruit", update_fruit);
+
+    ArrayList<Fruit> fruits = fruitMapper.selectAllFruit();
+    model.addAttribute("fruits", fruits);
+    return "sample51.html";
+  }
+
+  @PostMapping("step5")
+  public String sample55(@RequestParam Integer id, @RequestParam String name, @RequestParam Integer price,
+      ModelMap model) {
+    System.out.println("step5");
+    System.out.println(id);
+    System.out.println(name);
+    System.out.println(price);
+    Fruit fruit = new Fruit(id, name, price);
+
+    fruitMapper.updateById(fruit);
+    ArrayList<Fruit> fruits = fruitMapper.selectAllFruit();
+    model.addAttribute("fruits", fruits);
+
     return "sample51.html";
   }
 
